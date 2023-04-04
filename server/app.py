a64f983121e67c4e89ec8054a224689ae8e019aa
Sarah Whalley <s.whalley1130@gmail.com>
2023-04-04 13:00:08 -0600

    Completes lab by creating models and routing correct information to be displayed from db

--- server/app.py
+++ server/app.py
@@ -19,15 +19,60 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal=Animal.query.filter(Animal.id==id).first()
+    if not animal:
+        rb='<h1>404 Animal not found</h1>'
+        r=make_response(r, 404)
+        return r
+    
+    rb=f'''<ul>Name: {animal.name}</ul>
+            <ul>Species: {animal.species}</ul>
+            <ul>Zookeeper: {animal.zookeeper.name}</ul>
+            <ul>Enclosure: {animal.enclosure.environment}</ul>'''
+    r=make_response(rb, 200)
+    return r
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    keeper=Zookeeper.query.filter(Zookeeper.id==id).first()
+    if not keeper:
+        rb='<h1>404 Zookeeper not found</h1>'
+        r=make_response(rb, 404)
+        return r
+    
+    rb=f"""<ul>Name: {keeper.name}</ul>
+        <ul>Birthday: {keeper.birthday}</ul>"""
+    
+    animals=[animal for animal in keeper.animals]
+    if not animals:
+        rb+=f'<ul>{keeper.name} doesn\'t have any animals'
+    else:
+        for a in animals:
+            rb+=f"<ul>Animal: {a.name} the {a.species}</ul>"
+
+    
+    r=make_response(rb,200)
+    return r
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    encl=Enclosure.query.filter(Enclosure.id==id).first()
+    if not encl:
+        rb=f'404 Enclosure not found'
+        r=make_response(rb,404)
+        return r
+    rb=f'''<ul>Environment: {encl.environment}</ul>
+        <ul>Open to Visitors: {bool(encl.open_to_visitors)}</ul>'''
+    
+    animals=[animal for animal in encl.animals]
+    if not animals:
+        rb+=f'<ul>This enclosure doesn\'t have any animals'
+    else:
+        for a in animals:
+            rb+=f"<ul>Animal: {a.name} the {a.species}</ul>"
+    
+    r=make_response(rb, 200)
+    return r
 
 
 if __name__ == '__main__':
---
+++ server/migrations/versions/0b6f632146d9_creates_animal_zookeeper_and_enclosure_.py
@@ -0,0 +1,24 @@
+"""Creates animal, zookeeper, and enclosure models
+
+Revision ID: 0b6f632146d9
+Revises: 
+Create Date: 2023-04-04 12:17:45.942346
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '0b6f632146d9'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
